0e35fc89e14a5898888c3c4dfc91de8dc5ff0bd9
rubem.oliota <rubem.oliota@benner.com.br>
2021-05-29 12:48:57 -0300

    aula 03 js

---
+++ cursos/proway/site_javascript/site/dist/material/exemplo_em_aula_javascript_aula_003/js/bootstrap_jquery.js
@@ -0,0 +1,3 @@
+document.writeln('<link rel="stylesheet" href="bootstrap/css/bootstrap.css">')
+document.writeln('<script src="./bootstrap/js/bootstrap.bundle.js">'+'<'+"/script>")
+document.writeln('<script src="./js/jquery.js">'+'<'+"/script>")
